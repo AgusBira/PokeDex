5dbf9a8b4bb3eb0670dd0a624e900cd55c712cb2
Agus Bira <agusbira398@gmail.com>
2025-03-29 11:10:19 -0300

    feat: uno funcion pokemons y obtenerpokemons , modificando detalles

--- components/PokeContainer.jsx/PokeContainer.jsx
+++ components/PokeContainer.jsx/PokeContainer.jsx
@@ -2,16 +2,13 @@ import React from 'react'
 import Image from 'next/image'
 import Card from '../Card/Card'
 
-async function obtenerPokemon(poke) {
-    let res = await fetch(`https://pokeapi.co/api/v2/pokemon/${poke}/`)
-    let data = res.json()
-    return data
-}
-async function pokemons() {
+async function obtenerPokemon(pokeStart) {
     let arrayPoke = []
-    for(let i = 1; i <= 26; i++){
+    for(let i = pokeStart; i <= (pokeStart+9); i++){
         let idPokemon = String(i)
-        let poke = await obtenerPokemon(idPokemon)
+        let res = await fetch(`https://pokeapi.co/api/v2/pokemon/${idPokemon}/`)
+        let poke = await res.json()
+        
         arrayPoke.push({
             name: poke.name,
             image: poke.sprites.front_default,
@@ -21,13 +18,15 @@ async function pokemons() {
             atks: poke.stats.find(stat => stat.stat.name === "special-attack").base_stat,
             defs: poke.stats.find(stat => stat.stat.name === "special-defense").base_stat,
             types: poke.types.map(type => type.type.name),
-            /* typeImg:`https://play.pokemonshowdown.com/sprites/types/${types}.png` */
         })
     }
     return arrayPoke
 }
+async function pokemons() {
+    
+}
 export default async function PokeContainer() {
-let data = await pokemons()
+let data = await obtenerPokemon(1)
 console.log(data)
 return (
     <div className='flex flex-wrap gap-10 justify-center mt-10'>
